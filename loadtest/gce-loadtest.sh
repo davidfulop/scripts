62de03af8155eacf63b46858182e4acfa871cce1
David Fulop <28536613+davidfulop@users.noreply.github.com>
2019-10-16 17:57:33 +0100

    Generate report from test run

--- loadtest/gce-loadtest.sh
+++ loadtest/gce-loadtest.sh
@@ -6,6 +6,8 @@ INSTANCE_NAME=load-testing-instance
 GCP_PROJECT=$1
 ARTILLERY_YML=$2
 REMOTE_YML_PATH="/tmp/$ARTILLERY_YML"
+LOCAL_RESULT=$ARTILLERY_YML"_results_"$(date +%s)".json"
+REMOTE_RESULT_PATH="/tmp/"$LOCAL_RESULT
 
 echo "Creating instance: $INSTANCE_NAME"
 gcloud compute instances create $INSTANCE_NAME \
@@ -22,6 +24,10 @@ gcloud compute ssh --project $GCP_PROJECT --zone europe-west1-b $INSTANCE_NAME -
     && sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y \
     && sudo apt install -y nodejs npm \
     && npm install --ignore-scirpts artillery \
-    && echo 'Starting load test... \
-    && node ~/node_modules/artillery/bin/artillery run $REMOTE_YML_PATH \
-    && wait"
+    && echo 'Starting load test...' \
+    && node ~/node_modules/artillery/bin/artillery run $REMOTE_YML_PATH -o $REMOTE_RESULT_PATH"
+
+echo "Copying result file to local machine..."
+gcloud compute scp --zone europe-west1-b $INSTANCE_NAME:$REMOTE_RESULT_PATH $(pwd)"/"$LOCAL_RESULT
+echo "Creating report..."
+artillery report $LOCAL_RESULT
