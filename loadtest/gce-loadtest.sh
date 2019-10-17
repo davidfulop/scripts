21aa645dffa8038d6cd668c2923aa08adcf48076
David Fulop <28536613+davidfulop@users.noreply.github.com>
2019-10-17 22:19:46 +0100

    Clean up, refactor a bit

--- loadtest/gce-loadtest.sh
+++ loadtest/gce-loadtest.sh
@@ -6,22 +6,23 @@ INSTANCE_NAME=load-testing-instance
 GCP_PROJECT=$1
 ARTILLERY_YML=$2
 DATE=$(date +%s)
-LOCAL_RESULT_DIR=$(pwd)"/results"
+LOCAL_RESULT_DIR="./results"
 REMOTE_YML_PATH="/tmp/$ARTILLERY_YML"
-RESULT_PATH=$LOCAL_RESULT_DIR"/"$ARTILLERY_YML"_results_"$DATE".json"
-REPORT_PATH=$LOCAL_RESULT_DIR"/"$ARTILLERY_YML"_results_"$DATE".html"
-REMOTE_RESULT_PATH="/tmp/"$ARTILLERY_YML"_results_"$DATE".json"
-REMOTE_REPORT_PATH="/tmp/"$ARTILLERY_YML"_results_"$DATE".html"
+RESULT_FILE_NAME=$ARTILLERY_YML"_results_"$DATE
+RESULT_PATH=$LOCAL_RESULT_DIR"/"$RESULT_FILE_NAME".json"
+REPORT_PATH=$LOCAL_RESULT_DIR"/"$RESULT_FILE_NAME".html"
+REMOTE_RESULT_PATH="/tmp/"$RESULT_FILE_NAME".json"
+REMOTE_REPORT_PATH="/tmp/"$RESULT_FILE_NAME".html"
 
 echo "Creating instance: $INSTANCE_NAME"
 gcloud compute instances create $INSTANCE_NAME \
     --image-family ubuntu-1904 \
     --image-project ubuntu-os-cloud \
     --machine-type n1-highcpu-8 \
-     --preemptible
+    --preemptible
 
-echo "Copying" $(pwd)"/"$ARTILLERY_YML "to" $REMOTE_YML_PATH
-gcloud compute scp --zone europe-west1-b $(pwd)"/"$ARTILLERY_YML $INSTANCE_NAME:$REMOTE_YML_PATH
+echo "Copying" $ARTILLERY_YML "to" $REMOTE_YML_PATH
+gcloud compute scp --zone europe-west1-b $ARTILLERY_YML $INSTANCE_NAME:$REMOTE_YML_PATH
 
 echo "Connecting to instance..."
 gcloud compute ssh --project $GCP_PROJECT --zone europe-west1-b $INSTANCE_NAME -- \
